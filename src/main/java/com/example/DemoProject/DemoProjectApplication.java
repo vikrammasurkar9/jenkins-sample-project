415d963fa210d61eca7f51ff5432f333626781c7
Vikram Masurkar <129715801+vikrammasurkar9@users.noreply.github.com>
2023-07-20 15:19:21 +0530

    Update DemoProjectApplication.java

--- src/main/java/com/example/DemoProject/DemoProjectApplication.java
+++ src/main/java/com/example/DemoProject/DemoProjectApplication.java
@@ -8,6 +8,7 @@ public class DemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
+		System.out.println("Welcome to Jenkins.....");
 	}
 
 }
